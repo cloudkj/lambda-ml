f17af905d4e40d4da682f4b22b28c7b5ae8474cd
cloudkj <cloudkj@users.noreply.github.com>
2015-08-11 00:33:29 -0700

    Add test cases for k nearest neighbors

--- test/small/neighborhood_test.clj
+++ test/small/neighborhood_test.clj
@@ -20,6 +20,57 @@
     (is (= [4 8] (pq/item-value (first (knn 5 [3 9])))))))
 
 (deftest test-knn3
+  (let [points {[0.0   0.0]    1
+                [10.1  -10.1]  2
+                [-12.2 12.2]   3
+                [38.3  38.3]   4
+                [79.99 179.99] 5}
+        knn (make-knn (keys points))]
+    (is (= (list 1 2 3 4)
+           (map (comp points second) (knn 4 ((map-invert points) 1)))))
+    (is (= (list 2 1 3 4)
+           (map (comp points second) (knn 4 ((map-invert points) 2)))))
+    (is (= (list 3 1 2 4)
+           (map (comp points second) (knn 4 ((map-invert points) 3)))))
+    (is (= (list 4 1 2 3)
+           (map (comp points second) (knn 4 ((map-invert points) 4)))))
+    (is (= (list 5 4 3 1)
+           (map (comp points second) (knn 4 ((map-invert points) 5)))))))
+
+(deftest test-knn4
+  (let [points {[0.436697697345292 0.492281587956396] 1
+                [0.318000697283004 0.302602867518914] 2
+                [0.268674100320323 0.684132163547525] 3
+                [0.347190228888873 0.959920716313895] 4
+                [0.539212291014011 0.187100169547265] 5
+                [0.964631186098456 0.129079314315528] 6
+                [0.171792010609788 0.795749621321345] 7
+                [0.910157297130659 0.437962722965556] 8
+                [0.847975159955406 0.169625495659256] 9
+                [0.793504465072615 0.121750314432942] 10}
+        knn (make-knn (keys points))]
+    (is (= (list 1 2 3 5)
+           (map (comp points second) (knn 4 ((map-invert points) 1)))))
+    (is (= (list 2 1 5 3)
+           (map (comp points second) (knn 4 ((map-invert points) 2)))))
+    (is (= (list 3 7 1 4)
+           (map (comp points second) (knn 4 ((map-invert points) 3)))))
+    (is (= (list 4 7 3 1)
+           (map (comp points second) (knn 4 ((map-invert points) 4)))))
+    (is (= (list 5 2 10 9)
+           (map (comp points second) (knn 4 ((map-invert points) 5)))))
+    (is (= (list 6 9 10 8)
+           (map (comp points second) (knn 4 ((map-invert points) 6)))))
+    (is (= (list 7 3 4 1)
+           (map (comp points second) (knn 4 ((map-invert points) 7)))))
+    (is (= (list 8 9 6 10)
+           (map (comp points second) (knn 4 ((map-invert points) 8)))))
+    (is (= (list 9 10 6 8)
+           (map (comp points second) (knn 4 ((map-invert points) 9)))))
+    (is (= (list 10 9 6 5)
+           (map (comp points second) (knn 4 ((map-invert points) 10)))))))
+
+(deftest test-knn5
   (let [points {[36.971838 -122.019653] :SantaCruz,
                 [37.864012 -122.277832] :Berkeley,
                 [37.330857 -121.887817] :SanJose,
