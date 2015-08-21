3c247130ac71c5c7a1b52672a35586ae464b81f1
cloudkj <cloudkj@users.noreply.github.com>
2015-08-20 22:00:45 -0700

    Add naive Bayes implementation

---
+++ src/small/naive_bayes.clj
@@ -0,0 +1,54 @@
+(ns small.naive-bayes
+  (require [clojure.math.numeric-tower :refer :all]))
+
+;; Naive Bayes
+
+(defn gaussian
+  [x mean var]
+  (* (/ 1 (sqrt (* 2 Math/PI var)))
+     (expt Math/E (- (/ (expt (- x mean) 2) (* 2 var))))))
+
+(defn posterior
+  [model xi label]
+  (if-not (vector? xi)
+    (posterior model (vec xi) label)
+    (loop [index 0
+           prob (/ 1 (count model))]
+      (if (>= index (count xi))
+        prob
+        (let [[mean var] (get-in model [label index])]
+          (recur (inc index)
+                 (* prob (gaussian (nth xi index) mean var))))))))
+
+(defn naive-bayes-fit
+  "Returns a naive Bayes model fit to the given training data."
+  ([data]
+   (naive-bayes-fit (map butlast data) (map last data)))
+  ([x y]
+   (cond
+     (not-every? vector? x) (naive-bayes-fit (map vec x) y)
+     (not (vector? y)) (naive-bayes-fit x (vec y))
+     :else
+     (let [n (count (first x))]
+       (loop [index 0
+              labels (distinct y)
+              model {}]
+         (cond (empty? labels) model
+               (>= index n) (recur 0 (rest labels) model)
+               :else
+               (let [label (first labels)
+                     ;; Feature values for examples with the current label
+                     vals (->> (map #(nth % index) x)
+                               (keep-indexed (fn [i xi] (when (= (nth y i) label) xi))))
+                     mean (/ (apply + vals) (count vals))
+                     ;; Unbiased sample variance
+                     var (/ (apply + (map #(expt (- % mean) 2) vals)) (dec (count vals)))]
+                 (recur (inc index)
+                        labels
+                        (assoc-in model [label index] [mean var])))))))))
+
+(defn naive-bayes-predict
+  "Predicts the values of example data using a naive Bayes model."
+  [model x]
+  (let [labels (keys model)]
+    (map (fn [xi] (apply max-key #(posterior model xi %) labels)) x)))
---
+++ test/small/naive_bayes_test.clj
@@ -0,0 +1,25 @@
+(ns small.naive-bayes-test
+  (:require [clojure.test :refer :all]
+            [small.naive-bayes :refer :all]))
+
+(deftest test-naive-bayes
+  (let [data [[:male   6.00 180 12]
+              [:male   5.92 190 11]
+              [:male   5.58 170 12]
+              [:male   5.92 165 10]
+              [:female 5.00 100 6]
+              [:female 5.50 150 8]
+              [:female 5.42 130 7]
+              [:female 5.75 150 9]]
+        model (naive-bayes-fit (map #(subvec % 1) data)
+                               (map first data))]
+    (let [[mean variance] (get-in model [:male 0])]
+      (is (<= (- 5.855 mean) 10E-6))
+      (is (<= (- 3.5033E-2 variance) 10E-6)))
+    (let [[mean variance] (get-in model [:female 1])]
+      (is (<= (- 132.5 mean) 10E-6))
+      (is (<= (- 5.5833E+2 variance) 10E-6)))
+    (let [[mean variance] (get-in model [:male 2])]
+      (is (<= (- 11.25 mean) 10E-6))
+      (is (<= (- 9.1667E-1 variance) 10E-6)))
+    (is (= :female (first (naive-bayes-predict model [[6.0 130 8]]))))))
