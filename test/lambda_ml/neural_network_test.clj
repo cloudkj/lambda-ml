3588cea68b38cca34fb78ca533a3a9a6e730f078
cloudkj <cloudkj@users.noreply.github.com>
2016-04-08 20:08:21 -0700

    Add neural network unit test

---
+++ test/lambda_ml/neural_network_test.clj
@@ -0,0 +1,15 @@
+(ns lambda-ml.neural-network-test
+  (:require [clojure.test :refer :all]
+            [lambda-ml.neural-network :refer :all]))
+
+(deftest test-feed-forward
+  (let [weights [[[0.35 0.15 0.20]
+                  [0.35 0.25 0.30]]
+                 [[0.60 0.40 0.45]
+                  [0.60 0.50 0.55]]]
+        x [0.05 0.1]
+        [hidden output] (feed-forward x weights)]
+    (is (< (Math/abs (- 0.593269920 (first hidden))) 1E-6))
+    (is (< (Math/abs (- 0.596884378 (second hidden))) 1E-6))
+    (is (< (Math/abs (- 0.751365070 (first output))) 1E-6))
+    (is (< (Math/abs (- 0.772928465 (second output))) 1E-6))))
