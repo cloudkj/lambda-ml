c8669b0553f8db8a4c83edca21caacb64b7c3444
cloudkj <cloudkj@users.noreply.github.com>
2016-12-21 01:16:01 -0800

    Refactor naive Bayes interface

--- src/lambda_ml/naive_bayes.clj
+++ src/lambda_ml/naive_bayes.clj
@@ -9,32 +9,32 @@
      (expt Math/E (- (/ (expt (- x mean) 2) (* 2 var))))))
 
 (defn posterior
-  [model xi label]
+  [distributions xi label]
   (if-not (vector? xi)
-    (posterior model (vec xi) label)
+    (posterior distributions (vec xi) label)
     (loop [index 0
-           prob (/ 1 (count model))]
+           prob (/ 1 (count distributions))]
       (if (>= index (count xi))
         prob
-        (let [[mean var] (get-in model [label index])]
+        (let [[mean var] (get-in distributions [label index])]
           (recur (inc index)
                  (* prob (gaussian (nth xi index) mean var))))))))
 
 (defn naive-bayes-fit
   "Returns a naive Bayes model fit to the given training data."
-  ([data]
-   (naive-bayes-fit (map butlast data) (map last data)))
-  ([x y]
+  ([model data]
+   (naive-bayes-fit model (map butlast data) (map last data)))
+  ([model x y]
    (cond
-     (not-every? vector? x) (naive-bayes-fit (map vec x) y)
-     (not (vector? y)) (naive-bayes-fit x (vec y))
+     (not-every? vector? x) (naive-bayes-fit model (map vec x) y)
+     (not (vector? y))      (naive-bayes-fit model x (vec y))
      :else
      (let [n (count (first x))]
        (loop [index 0
               labels (distinct y)
-              model {}]
-         (cond (empty? labels) model
-               (>= index n) (recur 0 (rest labels) model)
+              m {}]
+         (cond (empty? labels) (assoc model :distributions m)
+               (>= index n)    (recur 0 (rest labels) m)
                :else
                (let [label (first labels)
                      ;; Feature values for examples with the current label
@@ -45,10 +45,16 @@
                      var (/ (apply + (map #(expt (- % mean) 2) vals)) (dec (count vals)))]
                  (recur (inc index)
                         labels
-                        (assoc-in model [label index] [mean var])))))))))
+                        (assoc-in m [label index] [mean var])))))))))
 
 (defn naive-bayes-predict
   "Predicts the values of example data using a naive Bayes model."
   [model x]
-  (let [labels (keys model)]
-    (map (fn [xi] (apply max-key #(posterior model xi %) labels)) x)))
+  (let [{distributions :distributions} model
+        labels (keys distributions)]
+    (map (fn [xi] (apply max-key #(posterior distributions xi %) labels)) x)))
+
+(defn make-naive-bayes
+  "Returns a naive Bayes model."
+  []
+  {})
--- test/lambda_ml/naive_bayes_test.clj
+++ test/lambda_ml/naive_bayes_test.clj
@@ -11,15 +11,15 @@
               [:female 5.50 150 8]
               [:female 5.42 130 7]
               [:female 5.75 150 9]]
-        model (naive-bayes-fit (map #(subvec % 1) data)
-                               (map first data))]
-    (let [[mean variance] (get-in model [:male 0])]
+        model (make-naive-bayes)
+        fit (naive-bayes-fit model (map #(subvec % 1) data) (map first data))]
+    (let [[mean variance] (get-in (:distributions fit) [:male 0])]
       (is (<= (- 5.855 mean) 10E-6))
       (is (<= (- 3.5033E-2 variance) 10E-6)))
-    (let [[mean variance] (get-in model [:female 1])]
+    (let [[mean variance] (get-in (:distributions fit) [:female 1])]
       (is (<= (- 132.5 mean) 10E-6))
       (is (<= (- 5.5833E+2 variance) 10E-6)))
-    (let [[mean variance] (get-in model [:male 2])]
+    (let [[mean variance] (get-in (:distributions fit) [:male 2])]
       (is (<= (- 11.25 mean) 10E-6))
       (is (<= (- 9.1667E-1 variance) 10E-6)))
-    (is (= :female (first (naive-bayes-predict model [[6.0 130 8]]))))))
+    (is (= :female (first (naive-bayes-predict fit [[6.0 130 8]]))))))
