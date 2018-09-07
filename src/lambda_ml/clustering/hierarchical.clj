353f7e62674fc6cb37995f98662388073b2bb330
cloudkj <cloudkj@users.noreply.github.com>
2018-09-06 22:20:04 -0700

    Initial implementation of agglomerative clustering

--- src/lambda_ml/clustering/hierarchical.clj
+++ src/lambda_ml/clustering/hierarchical.clj
@@ -21,3 +21,45 @@
                               (pmap/priority-map-keyfn :distance))
                       (assoc queues i)))
                {})))
+
+(defn single-link
+  [distances x y z]
+  (min (get-in distances [x y :distance])
+       (get-in distances [x z :distance])))
+
+(defn agglomerative-clustering
+  [f points]
+  (loop [distances (pairwise-distances f points)
+         queues (distance-queues distances)
+         active (reduce #(assoc %1 %2 true) {} (range (count points)))
+         merges []]
+    (if (<= (count active) 1)
+      merges
+      (let [;; Find the two most similar clusters
+            [_ k1 k2] (->> (keys active)
+                           (reduce (fn [[min-dist k1 k2] i]
+                                     (let [[k {dist :distance}] (peek (get queues i))]
+                                       (if (< dist min-dist)
+                                         [dist i k]
+                                         [min-dist k1 k2])))
+                                   [Double/MAX_VALUE nil nil]))
+            ;; Clear queue for k1
+            queues (assoc queues k1 (pmap/priority-map-keyfn :distance))
+            ;; Update distances
+            [distances queues] (->> (keys active)
+                                    (filter #(and (not (= k1 %)) (not (= k2 %))))
+                                    (reduce (fn [[d q] i]
+                                              ;; TODO: specify link function
+                                              (let [dist (single-link distances i k1 k2)
+                                                    d (-> (assoc-in d [i k1 :distance] dist)
+                                                          (assoc-in [k1 i :distance] dist))
+                                                    q (-> (update q i #(dissoc % k1))
+                                                          (update i #(dissoc % k2))
+                                                          (update i #(assoc % k1 (get-in d [i k1])))
+                                                          (update k1 #(assoc % i (get-in d [k1 i]))))]
+                                                [d q]))
+                                            [distances queues]))]
+        (recur distances
+               queues
+               (dissoc active k2)         ;; Deactivate cluster k2
+               (conj merges [k1 k2])))))) ;; Merge k1 and k2
--- test/lambda_ml/clustering/hierarchical_test.clj
+++ test/lambda_ml/clustering/hierarchical_test.clj
@@ -81,3 +81,49 @@
     (is (= 0 (first (second (get queues 1)))))
     (is (= 0 (first (first (get queues 2)))))
     (is (= 1 (first (second (get queues 2)))))))
+
+(deftest test-agglomerative-clustering
+  (let [distances {"BA" {"BA" 0   "FI" 662 "MI" 877 "NA" 255 "RM" 412 "TO" 996}
+                   "FI" {"BA" 662 "FI" 0   "MI" 295 "NA" 468 "RM" 268 "TO" 400}
+                   "MI" {"BA" 877 "FI" 295 "MI" 0   "NA" 754 "RM" 564 "TO" 138}
+                   "NA" {"BA" 255 "FI" 468 "MI" 754 "NA" 0   "RM" 219 "TO" 869}
+                   "RM" {"BA" 412 "FI" 268 "MI" 564 "NA" 219 "RM" 0   "TO" 669}
+                   "TO" {"BA" 996 "FI" 400 "MI" 138 "NA" 869 "RM" 669 "TO" 0}}
+        f (fn [a b] (get-in distances [a b]))
+        merges (agglomerative-clustering f (keys distances))]
+    (is (= [2 5] (nth merges 0)))
+    (is (= [3 4] (nth merges 1)))
+    (is (= [0 3] (nth merges 2)))
+    (is (= [0 1] (nth merges 3)))
+    (is (= [0 2] (nth merges 4)))))
+
+(deftest test-agglomerative-clustering2
+  (let [points [[1 1 1 0 1 0 0 1 1 1]
+                [1 1 0 1 1 0 0 0 0 1]
+                [0 1 1 0 1 0 0 1 0 0]
+                [0 0 0 1 0 1 0 0 0 0]
+                [1 1 1 0 1 0 1 1 1 0]
+                [0 1 0 1 1 0 0 0 0 1]
+                [0 1 1 0 1 1 0 1 1 0]]
+        merges (agglomerative-clustering d/jaccard points)]
+    (is (= [1 5] (nth merges 0)))
+    (is (= [0 4] (nth merges 1)))
+    (is (= [2 6] (nth merges 2)))
+    (is (= [0 2] (nth merges 3)))
+    (is (= [0 1] (nth merges 4)))
+    (is (= [0 3] (nth merges 5)))))
+
+(deftest test-agglomerative-clustering3
+  (let [distances {"A" {"A" 0.00 "B" 0.71 "C" 5.66 "D" 3.61 "E" 4.24 "F" 3.20}
+                   "B" {"A" 0.71 "B" 0.00 "C" 4.95 "D" 2.92 "E" 3.54 "F" 2.50}
+                   "C" {"A" 5.66 "B" 4.95 "C" 0.00 "D" 2.24 "E" 1.41 "F" 2.50}
+                   "D" {"A" 3.61 "B" 2.92 "C" 2.24 "D" 0.00 "E" 1.00 "F" 0.50}
+                   "E" {"A" 4.24 "B" 3.54 "C" 1.41 "D" 1.00 "E" 0.00 "F" 1.12}
+                   "F" {"A" 3.20 "B" 2.50 "C" 2.50 "D" 0.50 "E" 1.12 "F" 0.00}}
+        f (fn [a b] (get-in distances [a b]))
+        merges (agglomerative-clustering f (keys distances))]
+    (is (= [3 5] (nth merges 0)))
+    (is (= [0 1] (nth merges 1)))
+    (is (= [3 4] (nth merges 2)))
+    (is (= [2 3] (nth merges 3)))
+    (is (= [0 2] (nth merges 4)))))
