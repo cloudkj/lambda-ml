8aac380910202b5e135f277823fb2ef0d144f2cf
cloudkj <cloudkj@users.noreply.github.com>
2018-10-13 02:14:28 -0700

    Update project dependencies

--- project.clj
+++ project.clj
@@ -6,13 +6,15 @@
   :plugins [[lein-ancient "0.6.15"]
             [lein-codox "0.10.1"]
             [lein-exec "0.3.6"]
-            [lein-gorilla "0.3.6"]]
+            [lein-gorilla "0.4.0"]]
   :codox {:metadata {:doc/format :markdown}
           :namespaces [#"^lambda-ml\.(?!examples)"]
           :source-uri "https://github.com/cloudkj/lambda-ml/blob/master/{filepath}#L{line}"}
+  :jvm-opts ["-Xmx8g"]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/data.priority-map "0.0.10"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [gorilla-plot "0.1.4"]
-                 [net.mikera/core.matrix "0.61.0"]
+                 [net.mikera/core.matrix "0.62.0"]
                  [net.mikera/vectorz-clj "0.47.0"]])
