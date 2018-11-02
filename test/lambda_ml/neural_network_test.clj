ae87a37afbc3867efa9f18e1149f58cbb606c817
cloudkj <cloudkj@users.noreply.github.com>
2018-11-01 21:51:44 -0700

    Add unit test assertions

--- test/lambda_ml/neural_network_test.clj
+++ test/lambda_ml/neural_network_test.clj
@@ -1,5 +1,6 @@
 (ns lambda-ml.neural-network-test
   (:require [clojure.test :refer :all]
+            [clojure.core.matrix :as m]
             [lambda-ml.core :refer :all]
             [lambda-ml.neural-network :refer :all]))
 
@@ -49,9 +50,19 @@
   (let [x [0.05 0.1]
         activations [[0.593269920 0.596884378] [0.751365070 0.772928465]]
         errors [[0.00877136 0.00995425] [0.13849856 -0.03809824]]
-        g (compute-gradients x activations errors)]
-    ;; TODO: add assertions
-    (println g)))
+        [g0 g1] (compute-gradients x activations errors)]
+    (is (< (Math/abs (- 0.00877136 (nth (nth g0 0) 0))) 1E-6))
+    (is (< (Math/abs (- 0.00043857 (nth (nth g0 0) 1))) 1E-6))
+    (is (< (Math/abs (- 0.00087713 (nth (nth g0 0) 2))) 1E-6))
+    (is (< (Math/abs (- 0.00995425 (nth (nth g0 1) 0))) 1E-6))
+    (is (< (Math/abs (- 0.00049771 (nth (nth g0 1) 1))) 1E-6))
+    (is (< (Math/abs (- 0.00099543 (nth (nth g0 1) 2))) 1E-6))
+    (is (< (Math/abs (- 0.13849856 (nth (nth g1 0) 0))) 1E-6))
+    (is (< (Math/abs (- 0.08216703 (nth (nth g1 0) 1))) 1E-6))
+    (is (< (Math/abs (- 0.08266763 (nth (nth g1 0) 2))) 1E-6))
+    (is (< (Math/abs (- -0.03809824 (nth (nth g1 1) 0))) 1E-6))
+    (is (< (Math/abs (- -0.02260254 (nth (nth g1 1) 1))) 1E-6))
+    (is (< (Math/abs (- -0.02274024 (nth (nth g1 1) 2))) 1E-6))))
 
 (deftest test-regularize
   (let [weights [[[0.35 0.15 0.20]
@@ -60,9 +71,19 @@
                   [0.60 0.50 0.55]]]
         alpha 0.5
         lambda 0.1
-        weights (regularize weights alpha lambda)]
-    ;; TODO: add assertions
-    (println weights)))
+        [r0 r1] (regularize weights alpha lambda)]
+    (is (< (Math/abs (- 0.0 (nth (nth r0 0) 0))) 1E-6))
+    (is (< (Math/abs (- 0.0075 (nth (nth r0 0) 1))) 1E-6))
+    (is (< (Math/abs (- 0.01 (nth (nth r0 0) 2))) 1E-6))
+    (is (< (Math/abs (- 0.0 (nth (nth r0 1) 0))) 1E-6))
+    (is (< (Math/abs (- 0.0125 (nth (nth r0 1) 1))) 1E-6))
+    (is (< (Math/abs (- 0.015 (nth (nth r0 1) 2))) 1E-6))
+    (is (< (Math/abs (- 0.0 (nth (nth r1 0) 0))) 1E-6))
+    (is (< (Math/abs (- 0.02 (nth (nth r1 0) 1))) 1E-6))
+    (is (< (Math/abs (- 0.0225 (nth (nth r1 0) 2))) 1E-6))
+    (is (< (Math/abs (- 0.0 (nth (nth r1 1) 0))) 1E-6))
+    (is (< (Math/abs (- 0.025 (nth (nth r1 1) 1))) 1E-6))
+    (is (< (Math/abs (- 0.0275 (nth (nth r1 1) 2))) 1E-6))))
 
 (deftest test-gradient-descent-step
   (let [weights [[[0.35 0.15 0.20]
@@ -87,13 +108,23 @@
     (is (< (Math/abs (- 0.561370121 (nth (nth w1 1) 2))) 1E-6))))
 
 (deftest test-init-parameters
-  (let [model {:layers [2 3 1]}
-        weights (init-parameters model)]
-    ;; TODO: add assertions on dimensions of weights
-    (println weights)
-    (doseq [m weights]
-      (println (clojure.core.matrix/shape m))
-      (clojure.core.matrix/pm m))))
+  (let [model {:layers [2 3 1] :seed 12345}
+        [w0 w1] (init-parameters model)]
+    (is (= [3 3] (m/shape w0)))
+    (is (= [1 4] (m/shape w1)))
+    (is (< (Math/abs (- -0.18780898 (m/mget w0 0 0))) 1E-6))
+    (is (< (Math/abs (- 0.58843630 (m/mget w0 0 1))) 1E-6))
+    (is (< (Math/abs (- 0.94880478 (m/mget w0 0 2))) 1E-6))
+    (is (< (Math/abs (- -0.49428072 (m/mget w0 1 0))) 1E-6))
+    (is (< (Math/abs (- -1.22341193 (m/mget w0 1 1))) 1E-6))
+    (is (< (Math/abs (- -0.69796098 (m/mget w0 1 2))) 1E-6))
+    (is (< (Math/abs (- -0.77722490 (m/mget w0 2 0))) 1E-6))
+    (is (< (Math/abs (- 2.06800870 (m/mget w0 2 1))) 1E-6))
+    (is (< (Math/abs (- -0.58734674 (m/mget w0 2 2))) 1E-6))
+    (is (< (Math/abs (- 0.46214534 (m/mget w1 0 0))) 1E-6))
+    (is (< (Math/abs (- 1.37458180 (m/mget w1 0 1))) 1E-6))
+    (is (< (Math/abs (- -0.09785321 (m/mget w1 0 2))) 1E-6))
+    (is (< (Math/abs (- -1.07643638 (m/mget w1 0 3))) 1E-6))))
 
 (deftest test-neural-network
   (let [data [[0 0 [0]]
